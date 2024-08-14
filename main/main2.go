608d4bda979851e54235402b471b75ae8365d42a
Piraya Sutthiparinyanon <a677248@AR8040677248N1.local>
2024-08-14 11:09:28 +0700

    after P'yod fixed

--- main/main2.go
+++ main/main2.go
@@ -17,27 +17,25 @@ type skill struct {
 	Tags        []string `json:"tags"`
 }
 
-var db *sql.DB
 
 func main() {
-	// connStr := "localhost://postgres:postgres@localhost:5432/demo?sslmode=disable"
-	connStr := "user=dev password=dev123 dbname=postgres sslmode=disable port=5432 host=127.0.0.1"
+	connStr := "postgres://dev:dev123@0.0.0.0:5432/postgres?sslmode=disable"
+	// connStr := "user=dev password=dev123 dbname=postgres sslmode=disable port=5432 host=127.0.0.1"
 	var err error
-	db, err = sql.Open("postgres", connStr)
+	db, err := sql.Open("postgres", connStr)
 	if err != nil {
-		fmt.Print("First")
 		log.Fatal(err)
 	}
 	// fmt.Print(db)
-	err = db.Ping()
-	if err != nil {
-		fmt.Print("Second")
-		log.Fatal(err)
-	}
+	// if err = db.Ping();err != nil {
+	// 	log.Fatal(err)
+	// }
 	fmt.Println("Succesfully connected to PostgreSQL!")
 	router := gin.Default()
 	router.GET("/ping", getPing)
-	router.GET("api/v1/skills", getAllSkills)
+
+	h := &handler{db:db}
+	router.GET("api/v1/skills", h.getAllSkills)
 	router.GET("/api/v1/skills/:key", getSkillById)
 
 	router.Run()
@@ -47,16 +45,37 @@ func getPing(context *gin.Context) {
 	context.JSON(200, gin.H{"message": "pong"})
 }
 
-func getAllSkills(context *gin.Context) {
+type handler struct{
+	db *sql.DB
+}
+
+func  (h *handler)getAllSkills(context *gin.Context) {
 	context.Header("Content-Type", "application/json")
 	// rows, err := db.Query(fmt.Sprintf("SELECT * FROM %q;", "skill"))
-	rows, err := db.Query("SELECT key, name, description, logo, tags FROM skill;")
+	// rows, err := db.Query("SELECT * FROM pg_catalog.pg_tables where schemaname = 'postgres';")
+	rows, err := h.db.Query("SELECT * FROM skill;")
+	// rows, err := db.Query("SELECT key, name, description, logo, tags FROM skill")
 	// rows, err := db.Query(fmt.Sprintf("SELECT key, name, description, logo, tags FROM %q;", "skill"))
+	
 	if err != nil {
 		log.Fatalf("Error querying database: %v", err)
 	}
+	// var count = 0
+	// for rows.Next(){
+	// 	count++
+	// }
+	var skills []skill
+	for rows.Next() {
+		var s skills
+		err := rows.Scan(&s.key, &s.name, &s.description, &s.logo, &s.tags)
+		if err != nil {
+			log.Fatal(err)
+		}
+		skills.append(s)
+	}
+	
 	defer rows.Close()
-	context.JSON(200, gin.H{"message": "pong"})
+	context.JSON(200, skill)
 }
 
 func getSkillById(context *gin.Context) {
