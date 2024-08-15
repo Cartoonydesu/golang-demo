4914bbe1a3331e018510391a57248d67215f1251
Piraya Sutthiparinyanon <a677248@AR8040677248N1.local>
2024-08-15 10:00:15 +0700

    suggest to edit + exercise

--- main/example.go
+++ main/example.go
@@ -1,17 +1,122 @@
 package main
 
 import (
-	"net/http"
+	// "net/http"
+	"fmt"
+	// "strings"
 
-	"github.com/gin-gonic/gin"
+	// "github.com/gin-gonic/gin"
 )
 
-func example() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run()
+func main() {
+	// r := gin.Default()
+	// r.GET("/ping", func(c *gin.Context) {
+	// 	c.JSON(http.StatusOK, gin.H{
+	// 		"message": "pong",
+	// 	})
+	// })
+	// r.Run()
+
+
+	// hello()
+
+	// fmt.Println(squareArea(128))
+
+	// fmt.Println(greet("Gopher"))
+
+	// name := "Gopher"
+	// greet2(&name)
+	// fmt.Println(name)
+	
+	list := [4]int{1, 3, 4, 2}
+	r := reverse(list)
+	fmt.Printf("%T %v\n", r, r)
+	fmt.Printf("%v === %v\n",list, reverseSlice(list))
+
+	// fmt.Println(sliceString("abcdef"))
+	// fmt.Println(sliceString("abcdefg"))
+
+	// test();
 }
+
+func greet(name string) string {
+	return fmt.Sprintf("Hello, %s", name)
+}
+
+func greet2(name *string) {
+	*name = fmt.Sprintf("Hello, %s", *name)
+// fmt.Println(*name)
+}
+
+func hello() {
+	fmt.Println("Hello")
+	fmt.Println("What is your name?: ")
+	var name string
+	fmt.Scanln(&name)
+	fmt.Printf("Hi %s.\n", name)
+}
+
+func reverse(arr [4]int) [4]int{
+	var result [4]int
+	i :=len(arr)-1
+	j := 0
+	for i>=0 {
+		result[j] = arr[i]
+		i--
+		j++
+	} 
+	return result
+}
+
+func reverseSlice(arr [4]int) []int {
+	var slice []int = arr[:]
+	var result []int
+	for len(slice) > 0 {
+		result = append(result, slice[len(slice)-1])
+		slice = slice[:len(slice)-1]
+		// break
+	}
+	return result
+	// return []int{arr[3], arr[2], arr[1], arr[0]}
+}
+
+func sliceString(s string) []string{
+	var result []string
+	// // i, j := 0,1
+	// // i:= 0
+	// sArr := strings.SplitAfter(s, "")
+	// fmt.Print(sArr)
+	// i:= 0
+	// for i< len(sArr)/2 {
+	// 	// fmt.Println(i, i+1)
+	// 	fmt.Println(sArr[i:i+2])
+	// 	result = append(result, sArr[i:i+2])
+	// 	// j++
+	// 	i = i+2
+	// }
+	// return result
+	for s+="*"; len(s)>1; s = s[2:] {
+		result = append(result, s[:2])
+	}
+	return result
+}
+
+func squareArea(a float64) float64 {
+	return a*a
+}
+
+func test() {
+	p:=10
+	var i = &p
+	var j = &i
+	fmt.Printf("%v %p %v\n", &i, i, *i)
+	fmt.Printf("j => %v %p %v %v\n", &j, j, *j, **j)
+	test2(i, j)
+}
+
+func test2(i *int, j **int) {
+	fmt.Printf("%v %p %v\n", &i, i, *i)
+	fmt.Printf("%v %p %v\n", &j, j, *j)
+	fmt.Printf("j => %v %p %v %v\n", &j, j, *j, **j)
+}
+
--- main/main2.go
+++ main/main2.go
@@ -1,94 +1,111 @@
 package main
 
-import (
-	"database/sql"
-	"fmt"
-	"log"
+// import (
+// 	"database/sql"
+// 	"fmt"
+// 	"log"
+// 	"net/http"
 
-	"github.com/gin-gonic/gin"
-	_ "github.com/lib/pq"
-)
+// 	"github.com/gin-gonic/gin"
+// 	"github.com/lib/pq"
+// 	_ "github.com/lib/pq"
+// )
 
-type skill struct {
-	Key         string   `json:"key"`
-	Name        string   `json:"name"`
-	Description string   `json:"description"`
-	Logo        string   `json:"logo"`
-	Tags        []string `json:"tags"`
-}
+// type skill struct {
+// 	Key         string   `json:"key"`
+// 	Name        string   `json:"name"`
+// 	Description string   `json:"description"`
+// 	Logo        string   `json:"logo"`
+// 	Tags        []string `json:"tags"`
+// }
 
 
-func main() {
-	connStr := "postgres://dev:dev123@0.0.0.0:5432/postgres?sslmode=disable"
-	// connStr := "user=dev password=dev123 dbname=postgres sslmode=disable port=5432 host=127.0.0.1"
-	var err error
-	db, err := sql.Open("postgres", connStr)
-	if err != nil {
-		log.Fatal(err)
-	}
-	// fmt.Print(db)
-	// if err = db.Ping();err != nil {
-	// 	log.Fatal(err)
-	// }
-	fmt.Println("Succesfully connected to PostgreSQL!")
-	router := gin.Default()
-	router.GET("/ping", getPing)
+// func main() {
+// 	connStr := "postgres://dev:dev123@localhost:5432/postgres?sslmode=disable"
+// 	var err error
+// 	db, err := sql.Open("postgres", connStr)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer db.Close()
+// 	// fmt.Print(db)
+// 	if err = db.Ping();err != nil {
+// 		log.Fatal(err)
+// 	}
 
-	h := &handler{db:db}
-	router.GET("api/v1/skills", h.getAllSkills)
-	router.GET("/api/v1/skills/:key", getSkillById)
+// 	// fmt.Println("Succesfully connected to PostgreSQL!")
+// 	router := gin.Default()
 
-	router.Run()
-}
+// 	h := &handler{db:db}
 
-func getPing(context *gin.Context) {
-	context.JSON(200, gin.H{"message": "pong"})
-}
+// 	router.GET("/ping", getPing)
+// 	router.GET("/api/v1/skills", h.getAllSkills)
+// 	router.GET("/api/v1/skills/:key", h.getSkillById)
+// 	router.POST("/api/v1/skills", h.postSkill)
+// 	router.Run()
+// }
 
-type handler struct{
-	db *sql.DB
-}
+// func getPing(context *gin.Context) {
+// 	context.JSON(http.StatusOK, gin.H{"message": "pong"})
+// }
 
-func  (h *handler)getAllSkills(context *gin.Context) {
-	context.Header("Content-Type", "application/json")
-	// rows, err := db.Query(fmt.Sprintf("SELECT * FROM %q;", "skill"))
-	// rows, err := db.Query("SELECT * FROM pg_catalog.pg_tables where schemaname = 'postgres';")
-	rows, err := h.db.Query("SELECT * FROM skill;")
-	// rows, err := db.Query("SELECT key, name, description, logo, tags FROM skill")
-	// rows, err := db.Query(fmt.Sprintf("SELECT key, name, description, logo, tags FROM %q;", "skill"))
-	
-	if err != nil {
-		log.Fatalf("Error querying database: %v", err)
-	}
-	// var count = 0
-	// for rows.Next(){
-	// 	count++
-	// }
-	var skills []skill
-	for rows.Next() {
-		var s skills
-		err := rows.Scan(&s.key, &s.name, &s.description, &s.logo, &s.tags)
-		if err != nil {
-			log.Fatal(err)
-		}
-		skills.append(s)
-	}
-	
-	defer rows.Close()
-	context.JSON(200, skill)
-}
+// type handler struct{
+// 	db *sql.DB
+// }
 
-func getSkillById(context *gin.Context) {
-	context.Header("Content-Type", "application/json")
-	paramkey := context.Param("key")
-	// rows, err := db.Query(fmt.Sprintf("SELECT * from skill where key = %v", paramkey))
-	// if err != nil {
-	// 	log.Panic("Error get skill by id!")
-	// }
-	// defer rows.Close()
+// func (h *handler) postSkill(context *gin.Context) {
+// 	var newSkill skill
+// 	err := context.BindJSON(&newSkill)
+// 	if err != nil {
+// 		context.JSON(http.StatusBadRequest, "Can not extract data from request body")
+// 		return
+// 	}
+// 	stmt, err := h.db.Prepare("INSERT INTO skill (key, name, description, logo, tags) VALUES ($1, $2, $3, $4, $5)")
+// 	if err != nil {
+// 		context.JSON(http.StatusBadRequest, gin.H{"message": "Can not create statement to create new skill"})
+// 		return
+// 	}
+// 	defer stmt.Close()
+// 	if _, err := stmt.Exec(newSkill.Key, newSkill.Name,	newSkill.Description, newSkill.Logo, pq.Array(newSkill.Tags)); err != nil {
+// 		context.JSON(http.StatusBadRequest, gin.H{"message": "Can not create new skill"})
+// 		return
+// 	}
+// 	context.JSON(http.StatusOK, newSkill)
+// }
 
-	// err = rows.Scan(&rows.key, &rows.name, )
-	context.JSON(200, gin.H{
-		"message": paramkey,
-	})
-}
+// func (h *handler) getAllSkills(context *gin.Context) {
+// 	rows, err := h.db.Query("SELECT key, name, description, logo, tags FROM skill;")
+// 	if err != nil {
+// 		context.JSON(http.StatusBadRequest, gin.H{"message": fmt.Sprintf("Can not get data from database")})
+// 		return
+// 	}
+// 	defer rows.Close()
+// 	var skills []skill
+// 	for rows.Next() {
+// 		var s skill
+// 		err := rows.Scan(&s.Key, &s.Name, &s.Description, &s.Logo, pq.Array(&s.Tags))
+// 		if err != nil {
+// 			context.JSON(http.StatusBadRequest, gin.H{"message": fmt.Sprintf("Can not get data from database")})
+// 			return
+// 		}
+// 		skills = append(skills, s)
+// 	}
+// 	context.JSON(http.StatusOK, skills)
+// }
+
+// func (h *handler) getSkillById(context *gin.Context) {
+// 	paramkey := context.Param("key")
+// 	row := h.db.QueryRow(fmt.Sprintf("SELECT key, name, description, logo, tags from skill where key = '%v';", paramkey))
+// 	var s skill
+// 	err := row.Scan(&s.Key, &s.Name, &s.Description, &s.Logo, pq.Array(&s.Tags))
+// 	if err != nil {
+// 		if err == sql.ErrNoRows{
+// 			context.JSON(http.StatusNotFound, gin.H{"message": fmt.Sprintf("Not found skill with the key %v", paramkey)})
+// 			return
+// 		}
+// 		context.JSON(http.StatusBadRequest, nil)
+// 		return
+// 	}
+// 	context.JSON(http.StatusOK, s)
+// }
+
